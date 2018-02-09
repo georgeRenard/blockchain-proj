18ead17ae19ad49bf4c13dfe8d706c852c5e08b6
georgeRenard <goshoangelov97@gmail.com>
2018-02-10 01:29:21 +0200

    Wallet structure and some code

---
+++ wallet/app/core/models/Mnemonics.js
@@ -0,0 +1,12 @@
+class Mnemonics {
+    
+    
+    
+    constructor(count){
+        this.count = count;
+        this.mnemonics = [];
+    }
+    
+}
+
+module.exports = Mnemonics;
--- wallet/main.js
+++ wallet/main.js
@@ -6,7 +6,7 @@ var win;
 
 function createWindow(){
     //Win w:800 h:800
-    win = new BrowserWindow({width: 500, height: 600});
+    win = new BrowserWindow({width: 450, height: 550, frame: false, resizable: false});
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'app/views/main.html'),
         protocol: 'file:',
@@ -15,7 +15,6 @@ function createWindow(){
     
     //Open dev tools
     win.webContents.openDevTools();
-    
     //Dereference the window
     win.on('closed', () => {
         win = null;
